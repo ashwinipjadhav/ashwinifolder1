d65cf9124aef4df265f8222c30e410f4207eb8be
ashwinipjadhav <ashwinipjadhav89@gmail.com>
2025-01-07 22:21:20 +0530

    practice

--- src/fisr2.java
+++ src/fisr2.java
@@ -3,5 +3,6 @@ public class fisr2 {
     public static void main(String[] ash){
         System.out.println("hushar ashu");
         System.out.println("hushar ashu");
+        System.out.println("hushar ashu");
     }
 }
