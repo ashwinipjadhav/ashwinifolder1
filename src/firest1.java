8f8e712778161b007f8d456abe2c4a203cdfa3e6
ashwinipjadhav <ashwinipjadhav89@gmail.com>
2025-01-07 23:53:31 +0530

    in fist1

--- src/firest1.java
+++ src/firest1.java
@@ -3,6 +3,10 @@ public class firest1 {
     public static void main(String[] args)
     {
         System.out.println("yedi ashu");
+        System.out.println(" ashu");
+        System.out.println("yedi ");
+        System.out.println("yedi ashu");
+
 
     }
 
