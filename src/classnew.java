6be284d8fc80455adbee532ed195c7a5af2b88a4
ashwinipjadhav <ashwinipjadhav89@gmail.com>
2025-01-12 10:39:32 +0530

    for loop

--- src/classnew.java
+++ src/classnew.java
@@ -1,5 +1,23 @@
 public class classnew {
     public static void main(String[] args) {
+     int num = 9;
+     //9*1=9,9*2=18,.....9*10=90
+        for(int i=1;i<11;i++){
+         int ans=9*i;
+         System.out.println(ans);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
