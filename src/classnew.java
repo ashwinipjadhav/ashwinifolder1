5264c7f311c0ea7f7535c1642501a68bedb67f93
ashwinipjadhav <ashwinipjadhav89@gmail.com>
2025-02-08 15:52:37 +0530

    new code trail

--- src/classnew.java
+++ src/classnew.java
@@ -1,15 +1,22 @@
 public class classnew {
     public static void main(String[] args) {
-     int num = 9;
-     //9*1=9,9*2=18,.....9*10=90
-        for(int i=1;i<11;i++){
-         int ans=9*i;
-         System.out.println(ans);
+        //int num = 9;
+        //9*1=9,9*2=18,.....9*10=90
+        // for(int i=1;i<11;i++)
 
-        }
+        int b = 54;
+        String Grades = (b >=90)?"A":(b>=80 && b<=89)?"B":(b>=70 && b<=79)?"C":(b>=60 && b<=69)?"D":"F";
+        System.out.println(Grades);
 
 
+    }
 
+/*
+    int m= 90 ;
+    String grade=(m>=90)?"A":(m>=80 && m<=89)?"B" :(m>=70 && m<=79)?"C":(m>=60 && m<=69)?"D":"F";
+
+    System.out.println(grade);
+ */
 
 
 
@@ -20,4 +27,4 @@ public class classnew {
 
 
     }
-}
+
